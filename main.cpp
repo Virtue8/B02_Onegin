df9107c991e84abd799353f178faa055d2cfc7d3
Virtue8 <virtue_8@mail.ru>
2024-09-20 16:50:44 +0300

    FileOpener + FileReader

--- main.cpp
+++ main.cpp
@@ -1,16 +1,4 @@
 
-//ZZN advice:
-//
-//char * all_chars (calloc(sizeoffile<-fstat (sizeof (char) !))) ask SDS
-//fread to read file to all_chars, return how_many_bytes_are_read?=x
-// num of strs  ('\n')
-//        ^           char ** str_adr   (also calloc(x, sizeofunit))     pointers of strings beginnings
-// '/n' to '/0' func
-// also 0th str address and +1 (cause de '\0')
-
-// then bubblesort str_adr
-
-
 //------------------------ Libraries and Files --------------------------//
 
 #include <stdio.h>
@@ -24,109 +12,76 @@
 
 //------------------------ Main Functions Init --------------------------//
 
-FILE * FileOpener (const char * file_stream);   // opens the stream
-
-//     ||
-//    \||/
-//     \/
-
-size_t FileSize (FILE * file);          // counts the amount of strings and meaning
-                                        // chars in every string of file
-//     ||
-//    \||/
-//     \/
-
-void FileToArray ();                    // 1) puts the reformed content
-                                        //    of the file to "text" array
-//     ||                               // 2) creates "index" array with
-//    \||/                              //    every string unique index
-//     \/                               //    (to sort in future)
-
-void BubbleSort (char ** text);
-
-//     ||                               // sorts indexes according to a-z (qSort in future)
-//    \||/
-//     \/
-
-void Output ();                         // writes the result in "output.txt"
-                                        // (currently it outputs the data in terminal)
-
-
-//---------------------- Secondary Functions Init -----------------------//
-
+FILE * FileOpener (const char * file_name);
+size_t FileReader (FILE * file_onegin);
+void Sorter (char ** text);
+void Output ();
 int CharComparator (char x, char y);
-void Output (string text[t_height], int index[t_height]);
 
-//----------------------------- Constants -------------------------------//
+//----------------------- Constants and Structs -------------------------//
 
-enum CharState
+const char * file_name = "text.txt"
+
+/*
+struct Onegin
 {
-    EQUAL,
-    BIGGER,
-    LESSER
+    FILE * file_onegin = NULL;
+    size_t file_size = 0;
+    char ** all_chars = 0;
+
+    size_t lines_amount = 0;
+
+    struct Line line_data = NULL;
 };
 
-enum BubbleSortStates
+struct Line
 {
-    REPEAT,
-    RIGHT
+    size_t line_len = 0;
+    char * line_place = 0;
 };
+*/
 
 //-------------------------------- main ---------------------------------//
 
 int main ()
 {
-    string text[t_height] = {};
-    int index[t_height] = {};
-    for (int i = 0; i < t_height; i++)
-        index[i] += i;                                 //makes index look like {0, 1, ..., 63}
-
-    fread (text, sizeof (char), t_height * t_length, file);
-
-    StringSorter (t_height, t_length, text, index);
-
-    Output(text, index);
+    FileOpener (file_name)
 }
 
 //--------------------------- Other functions --------------------------//
 
-FILE * FileOpener ()
-
-void BubbleSort (int t_height, int t_length, string text[t_height], int index[t_height])
+FILE * FileOpener (const char * file_name)
 {
-    int BubbleSortCriteria = REPEAT;
-    char x = '\0';
-    char y = '\0';
-
-    while (BubbleSortCriteria != RIGHT)
+    FILE * file_onegin = fopen (file_name, "r");
+    if (file_onegin == NULL)
     {
-
-        BubbleSortCriteria = RIGHT;
-
-        for (int i = 0; i <= t_height + 1; i++)
-        {
-            for (int j = 0; j <= t_length + 1; j++)
-            {
-                x = text[index[i]][j];
-                y = text[index[i+1]][j];
-
-                int CCV = CharComparator (x, y);
-
-                if (CCV == LESSER)
-                {
-                    int temp_sloth = index[i];
-                    index[i] = index[i+1];
-                    index[i+1] = temp_sloth;
-                    BubbleSortCriteria = REPEAT;
-                    break;
-                }
-                else if (CCV == EQUAL)
-                    continue;
-                else
-                    break;
-            }
-        }
+        printf ("Incorrect Input!\n");
+        exit ();
     }
+    else
+        return file_onegin;
+}
+
+//---------------------------------------------------------------------//
+
+size_t FileReader (FILE * file_onegin)
+{
+    fseek (file_onegin, 0, SEEK_SET);
+    file_size = fscanf (file_onegin);
+
+    char * all_chars = (char *) calloc (file_onegin, file_size);
+
+    // missing: getting all info for structs
+    // (not introduced yet, but the same parameters must be got here)
+}
+
+// missing: Sorter, Output
+
+//----------------------------------------------------------------------//
+
+void Sorter (char ** text)
+{
+
 }
 
 //----------------------------------------------------------------------//
@@ -147,6 +102,5 @@ int CharComparator (char x, char y)
 
 void Output (string text[t_height], int index[t_height])
 {
-    for (unsigned int i = 0; i <= sizeof(index) + 1; i++)
-        printf ("%d %s\n", i + 1, text[index[sizeof(index) + 1 - i]]);
+    .
 }
